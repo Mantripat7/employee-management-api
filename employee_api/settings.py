a570c64d9761ca5f0793c3e8bb8fa98c09585103
Mantripat Singh <135863711+Mantripat7@users.noreply.github.com>
2026-01-15 23:55:00 +0530

    Fix ALLOWED_HOSTS format in settings.py

--- employee_api/settings.py
+++ employee_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i55b^@+6#jsz346hkqj01k)gb3&!&j5ew4e03ab(hhh6!y8msp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
